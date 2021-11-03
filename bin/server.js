cc3cd29a4cc912e4f333ab60a82e1adb51618a4e
BokovRoman <r.bokov1991@gmail.com>
2021-11-03 23:09:18 +0200

    add .env to gitignore

--- bin/server.js
+++ bin/server.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose')
 
+const app = require('../app')
+
+require('dotenv').config()
+
+const { DB_HOST, PORT = 3000 } = process.env
+
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    app.listen(PORT)
+    console.log('Database connection successful')
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
+
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`)
+})
