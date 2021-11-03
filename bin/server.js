d2633ce7a0aca4596fa292927006a2bb890967f2
BokovRoman <r.bokov1991@gmail.com>
2021-11-03 22:22:21 +0200

    fixed updateContactbyID

--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,20 @@
+const mongoose = require('mongoose')
 const app = require('../app')
+
+const DB_HOST = 'mongodb+srv://Roman:8w6qL1oR9ZdcDCYA@cluster0.jdtlp.mongodb.net/db-contacts?retryWrites=true&w=majority'
+
+mongoose.connect(DB_HOST, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    app.listen(PORT)
+    console.log('Database connection successful')
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
 
 const PORT = process.env.PORT || 3000
 
