7d71227051aa5e75d6827d791b6327a8d0ccca54
BokovRoman <r.bokov1991@gmail.com>
2021-11-05 22:35:20 +0200

    created contactsSchemaJoi

--- models/contacts.js
+++ models/contacts.js
@@ -34,6 +34,26 @@ const contactSchema = Schema({
 { versionKey: false, timestamps: true }
 )
 
+const contactsSchemaJoi = Joi.object({
+  name: Joi.string()
+    .regex(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Name can only consist of letters, apostrophes, dashes and spaces.'
+    )
+    .required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .regex(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+  favorite: Joi.boolean(),
+})
+
 const Contact = model('contact', contactSchema)
 
-module.exports = Contact
+module.exports = {
+  contactsSchemaJoi,
+  Contact,
+}
