23b4d1f911d95c54b6f3a1028a2dc3b23dbdbc0a
BokovRoman <r.bokov1991@gmail.com>
2021-11-08 21:41:57 +0200

    adds routes/ auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const { auth: ctrl } = require('../controllers')
+const { controllerWrapper, validation } = require('../middlewares')
+
+router.post('/signup', controllerWrapper(ctrl.signup))
+
+router.post('/login', controllerWrapper(ctrl.login))
+
+router.get('/logout', controllerWrapper(ctrl.logout))
+
+router.get('/current', controllerWrapper(ctrl.current))
 
 module.exports = router
