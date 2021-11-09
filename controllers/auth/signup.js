6ad8517c1e959ae41de58e77d07f3713d0812fe1
BokovRoman <r.bokov1991@gmail.com>
2021-11-09 22:14:30 +0200

    adds signup in auth

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,5 +1,12 @@
-const signup = async(req, res) => {
+const { User } = require('../../models')
+const { Conflict } = require('http-errors')
 
+const signup = async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+  if (user) {
+    throw new Conflict('Email in use')
+  }
 }
 
 module.exports = signup
