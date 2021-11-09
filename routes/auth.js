2bd6807c6f56480a55c719f11073beece273c7cb
BokovRoman <r.bokov1991@gmail.com>
2021-11-09 21:47:22 +0200

    adds validation(userSchemaJoi),

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,15 @@ const router = express.Router()
 const { auth: ctrl } = require('../controllers')
 const { controllerWrapper, validation } = require('../middlewares')
 
-router.post('/signup', controllerWrapper(ctrl.signup))
+const { userSchemaJoi } = require('../models/user')
 
-router.post('/login', controllerWrapper(ctrl.login))
+router.post('/signup',
+  validation(userSchemaJoi),
+  controllerWrapper(ctrl.signup))
+
+router.post('/login',
+  validation(userSchemaJoi),
+  controllerWrapper(ctrl.login))
 
 router.get('/logout', controllerWrapper(ctrl.logout))
 
