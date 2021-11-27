83b189703d25c55d4b750677fda849c8d10a0242
BokovRoman <r.bokov1991@gmail.com>
2021-11-27 11:47:07 +0200

    Add change to signup and login controllers

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,6 +1,8 @@
 const { User } = require('../../models')
 const { Conflict } = require('http-errors')
-const { sendSuccessResponse } = require('../../utils')
+const { nanoid } = require('nanoid')
+
+const { sendSuccessResponse, sendEmail } = require('../../utils')
 
 const signup = async (req, res) => {
   const { email, password } = req.body
@@ -8,10 +10,23 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict('Email in use')
   }
+
   const newUser = new User({ email })
+  newUser.setVerifyToken(nanoid(10))
   newUser.setPassword(password)
-  const { subscription } = await newUser.save()
-  sendSuccessResponse(res, { email, subscription }, 201)
+
+  const { subscription, verifyToken } = await newUser.save()
+
+  const msg = {
+    to: email,
+    subject: 'Confirm Your Email',
+    html: `
+    <a href="http://localhost:3000/api/auth/verify/${verifyToken}" target="_blank">Let's confirm your email address</a>
+    `,
+  }
+
+  await sendEmail(msg)
+  sendSuccessResponse(res, { email, subscription, verifyToken }, 201)
 }
 
 module.exports = signup
